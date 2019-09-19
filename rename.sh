e7fe13c5a60eeca4c0e508047ba90078db25c02f
Francois Berenger <unixjunkie@sdf.org>
2019-09-19 16:21:41 +0900

    will work whatever the name prefix of all molecules in one of the files

--- rename.sh
+++ rename.sh
@@ -6,7 +6,7 @@ output=$3
 
 # prefix all actives with active_
 cat $ligands | \
-    perl -0777 -pe 's/MOLECULE\nTOX/MOLECULE\nactive_TOX/g' > $output
+    perl -0777 -pe 's/MOLECULE\n/MOLECULE\nactive_TOX/g' > $output
 # prefix all decoys with decoy_
 cat $decoys | \
-    perl -0777 -pe 's/MOLECULE\nTOX/MOLECULE\ndecoy_TOX/g' >> $output
+    perl -0777 -pe 's/MOLECULE\n/MOLECULE\ndecoy_TOX/g' >> $output
